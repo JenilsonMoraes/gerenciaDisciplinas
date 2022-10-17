20599408cb11488a86d15e3375cb1f04ecaf3022
JenilsonMoraes <jenilsinhomoraes@gmail.com>
2022-10-17 12:09:52 -0300

    Alteração no schema

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_17_031441) do
   create_table "alunos", force: :cascade do |t|
     t.string "nome"
     t.integer "turma_id", null: false
-
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["turma_id"], name: "index_alunos_on_turma_id"
