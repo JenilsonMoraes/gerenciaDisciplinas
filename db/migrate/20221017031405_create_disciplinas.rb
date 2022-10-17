02dfa74aa2d67caaa922f27b9e6b0da3d6a21698
JenilsonMoraes <jenilsinhomoraes@gmail.com>
2022-10-17 12:19:36 -0300

    Adicionando campo de turma no migrade de disciplina

--- db/migrate/20221017031405_create_disciplinas.rb
+++ db/migrate/20221017031405_create_disciplinas.rb
@@ -3,7 +3,7 @@ class CreateDisciplinas < ActiveRecord::Migration[7.0]
     create_table :disciplinas do |t|
       t.string :nome
       t.integer :anoLetivo
-
+      t.references :turma, null:false, foreign_key: true
       t.timestamps
     end
   end
