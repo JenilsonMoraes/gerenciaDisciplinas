b0057d19a7643f3707090b69616963d1a612048f
JenilsonMoraes <jenilsinhomoraes@gmail.com>
2022-10-17 23:26:08 -0300

    Configurando rotas e criando links na pagina principal

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'index#index'
   resources :turmas
   resources :disciplinas
   resources :avaliacaos
@@ -8,4 +9,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
 end
