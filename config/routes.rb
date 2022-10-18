91f162a72207cc54b64f76daad5d5710f24ea00c
JenilsonMoraes <jenilsinhomoraes@gmail.com>
2022-10-18 00:19:01 -0300

    Login criado

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  get "signup", to: "users#new"
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
+  resources :users, except: [:new]
+
   root 'index#index'
   resources :turmas
   resources :disciplinas
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_17_031441) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_18_023904) do
   create_table "alunos", force: :cascade do |t|
     t.string "nome"
     t.integer "turma_id", null: false
@@ -54,6 +54,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_17_031441) do
     t.string "letra"
     t.integer "ano"
     t.integer "anoLetivo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
